48eca6ecbe7d71f138e3e84577d5c22bffd3aae9
dinoguei <dinoguei@c1r8s13.42porto.com>
2022-11-30 16:25:20 +0000

    ✔

---
+++ get_next_line_utils.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dinoguei <dinoguei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 17:29:31 by dinoguei          #+#    #+#             */
+/*   Updated: 2022/11/22 15:19:21 by dinoguei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strjoin(char *str, const char *str2)
+{
+	char	*temp;
+	int		i;
+	int		len1;
+	int		len2;
+
+	if (!str || !str2)
+		return (NULL);
+	len1 = ft_strlen((char *)str);
+	len2 = ft_strlen ((char *)str2);
+	temp = malloc((len1 + len2 + 1) * sizeof(const char));
+	if (!temp)
+		return (NULL);
+	i = 0;
+	while (len1 > i)
+		temp[i++] = *str++;
+	i = 0;
+	while (len2 > i)
+		temp[len1 + i++] = *str2++;
+	temp[len1 + i] = '\0';
+	return (temp);
+}
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (src[j] != '\0')
+	{
+		j++;
+	}
+	if (size != '\0')
+	{
+		while (src[i] != '\0' && i < (size - 1))
+		{
+			dest[i] = src[i];
+			i++;
+		}
+	dest[i] = '\0';
+	}
+	return (j);
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	char	search;
+
+	search = c;
+	while (*str != search)
+	{
+		if (*str == '\0')
+			return (NULL);
+		str++;
+	}
+	return ((char *)str);
+}
